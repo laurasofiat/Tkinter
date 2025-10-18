d82bf0431c88b68d2ee08d6847fcf7ce7ea638a0
laurasofiat <torresmurillolaurasofia01@gmail.com>
2025-10-18 18:15:55 +0000

    holas

---
+++ Formulario_de_registro.py
@@ -0,0 +1,10 @@
+import Tkinter as tk
+ventana=tk.Tk()
+ventana.tittle("Registro de datos básicos")
+ventana.geometry("500*600")
+nombre=() 
+apellido=()
+for nom in nombre:
+    ing=input("Ingresa tus nombres: ").tk()
+    nombre+=ing
+    
